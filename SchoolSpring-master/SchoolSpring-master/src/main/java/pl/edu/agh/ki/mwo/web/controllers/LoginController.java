be79c07f4fd745719778e7efdd48ed3665526831
kubabono <46647760+kubabono@users.noreply.github.com>
2019-03-29 21:54:13 +0100

    Add files via upload

---
+++ SchoolSpring-master/SchoolSpring-master/src/main/java/pl/edu/agh/ki/mwo/web/controllers/LoginController.java
@@ -0,0 +1,31 @@
+package pl.edu.agh.ki.mwo.web.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//klasa obsługuje zapytania skierowane na webowy interfejs naszej aplikacji
+@Controller
+public class LoginController {
+
+    @RequestMapping(value={"/", "/Login"}, method=RequestMethod.GET)
+    public String displayLoginForm() {
+        return "loginForm";
+    }
+
+	@RequestMapping(value="/Login", method=RequestMethod.POST)
+	public String doLogin(@RequestParam(value="login") String login, Model model, HttpSession session) {
+		session.setAttribute("userLogin", login);
+		return "redirect:/Welcome";
+	}
+
+    @RequestMapping(value="/Welcome")
+    public String welcome(Model model, HttpSession session) {
+    	model.addAttribute("message", "Witamy w systemie Szkoła!");
+        return "welcome";
+    }
+}
